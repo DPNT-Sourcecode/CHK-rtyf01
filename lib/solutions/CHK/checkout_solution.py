38a0f6b3d6715374327c4d67f5c3a1c78ad6eecd
sbx_user1051 <sbx_user1051@169.254.25.217>
2024-06-15 15:21:44 +0000

    Sat Jun 15 15:21:44 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,18 @@
+# keys are the SKUs and the values are the prices.
+# Each price is a tuple of (quantity, total_price), sorted in descending order of quantity.
+prices = {
+    "A": [(3, 130), (1, 50)],
+    "B": [(2, 45), (1, 30)],
+    "C": [(1, 20)],
+    "D": [(1, 15)],
+}
+
+# set of valid SKUs
+catalogue = set(prices.keys())
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus: str) -> int:
     raise NotImplementedError()
+
